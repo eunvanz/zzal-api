dabc6a86a23ea882accee9dfeff9ee7cec898050
Benjamin <eunvanz@naver.com>
2021-10-11 16:33:05 +0900

    feat: create contents entities

---
+++ src/contents/contents.entity.ts
@@ -0,0 +1,49 @@
+import { TimeRecord } from 'src/entities/time-record.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Content extends TimeRecord {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: true })
+  userId: string;
+
+  @Column()
+  path: string;
+
+  @Column({ nullable: true })
+  title: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @OneToMany(() => Image, (image) => image.content, { eager: true })
+  images: Image[];
+}
+
+@Entity()
+export class Image extends TimeRecord {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  url: string;
+
+  @Column()
+  seq: number;
+
+  @Column()
+  contentId: number;
+
+  @ManyToOne(() => Content, { eager: true })
+  @JoinColumn({ name: 'content_id' })
+  content: Content;
+}
